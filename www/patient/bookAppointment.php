071a5877f0219db92f44368be3166e648c770dd2
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2019-11-29 19:24:26 -0700

    booking apt

---
+++ www/patient/bookAppointment.php
@@ -0,0 +1,10 @@
+<form action="bookAppointmentQuery.php" method="post">
+   Date: <input type="date" name="Date" min="2020-01-01" required><br>
+   Time: <input type="time" name="Time" min="06:00:00" max="18:00:00" step="1800" required><br>
+   Reason: <input type="text" name="Reason" required><br>
+   Doctor User ID: <input type="text" name="DoctorUserID"required><br>
+   <input type="submit" value="Submit">
+</form>
+<br>
+<a href="viewPatientInfo.php">Back</a>
+<br>
---
+++ www/patient/bookAppointmentQuery.php
@@ -0,0 +1,42 @@
+<?php
+// vars from form
+$date = $_POST["Date"];
+$time = $_POST["Time"];
+$reason = $_POST["Reason"];
+$docUID = $_POST["DoctorUserID"];
+//user var
+$UserID = $_SESSION['PatientID'];
+
+
+
+// Create connection
+$con=mysqli_connect("localhost","root","","mydb");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  //test query to make sure there is not already an appt at that time with same doc
+  $testq = mysqli_query($con,"SELECT * FROM APPOINTMENT AS A WHERE A.TIME='$time' AND A.DAPT_UserID_fk = '$docUID'");
+//check to see if there was anything returned by query  
+if (mysqli_num_rows($testq) < 1){
+  //if no try to insert
+  $sql = "INSERT INTO APPOINTMENT VALUES ('990087','$time','$date','$reason','$docUID');";
+ if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+  echo "Appointment successfully booked";
+}
+else {
+  //else tell them to try again
+  echo "$time, $date is unavailable with that doctor. Please try again.";
+}
+  
+
+
+mysqli_close($con);
+
+echo '<br> <a href="viewPatientInfo.php">Back</a> <br>';
+?>
