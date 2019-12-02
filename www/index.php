04bf661d678b4d25a04c44febb039f55639dfc86
Nathan Darby <nathan.darby1@ucalgary.ca>
2019-12-02 16:18:18 -0700

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,19 +1,13 @@
 <html>
 <body>
-
-<form action="add.php" method="post">
-   Name: <input type="text" name="name"><br>
-   E-mail: <input type="text" name="email"><br>
-   <input type="submit" value="login">
-</form>
 <br>
-<a href="admin-main.php">Be an Admin</a>
+<a href="admin/admin-main.php">Be an Admin</a>
 <br>
-<a href="doctor-main.php">Be a Doctor</a>
+<a href="doctor/doctor-main.php">Be a Doctor</a>
 <br>
-<a href="nurse-main.php">Be a Nurse</a>
+<a href="nurse/nurse-main.php">Be a Nurse</a>
 <br>
-<a href="patient-main.php">Be a Patient</a>
+<a href="patient/patient-main.php">Be a Patient</a>
 <br>
 <a href="index.php">New Patient? Create Account</a>
 <br>
