370909764676c92a7e04dfd091df5a2805bc5cca
Erslan <erss.99.es@gmail.com>
2019-12-03 19:56:01 -0700

    doctormain links

--- www/doctor/doctor-main.php
+++ www/doctor/doctor-main.php
@@ -1,8 +1,7 @@
 <html>
 <body>
 <h1>Welcome, Doctor</h1>
-<a href="index.php">View Patients</a><br>
-<a href="index.php">View Personal Information</a><br>
-<a href="index.php">View Prescriptions</a><br>
+<a href="viewPatientList.php">View Patients</a><br>
+<a href="viewDoctorInfo.php">View Personal Information</a><br>
 </body>
 </html>
