7e752f645a4f69723cef01847539c1db9e6f702a
Nathan Darby <nathan.darby1@ucalgary.ca>
2019-12-05 09:35:42 -0700

    UI for main page, fixed cancel buttons

--- www/index.php
+++ www/index.php
@@ -1,17 +1,51 @@
 <html>
+<style type="text/css">
+	body{
+		text-align: center;
+		padding-bottom: 100px;
+	}
+	#footer {
+   position:fixed;
+   bottom:0;
+   width:100%;
+   height:60px;
+   background:#6cf;
+}
+</style>
+
+<header>
+	<h1> Hospital Database Management System</h1>
+</header>
+
 <body>
 
+<img src="hospital-logo.jpg" alt="logo-main" height = "400" width = "400" align="center">
+<br><br><br>
+<strong><font size="+5">Login</font></strong>
+<br><br><br>
 
-<br>
-<a href="admin/login.php">Login as an Admin</a>
-<br>
-<a href="doctor/login.php">Login as a Doctor</a>
-<br>
-<a href="nurse/login.php">Login as a Nurse</a>
-<br>
-<a href="patient/login.php">Login as a Patient</a>
-<br>
-<a href="index.php">New Patient? Create Account</a>
-<br>
+
+<table style="vertical-align: bottom; margin-left: auto; margin-right: auto;">
+	<tr>
+		<th><img src="admin-login1.jpg" alt="admin-login1" height="200" width="200"></th>
+		<th><img src="doctor-login.jpg" alt="doctor-login" height="200" width="200"></th>
+		<th><img src="nurse-login.jpg" alt="nurse-login" height="200" width="200"></th>
+		<th><img src="patient-login.jpg" alt="patient-login1" height="200" width="200"></th>
+	</tr><br>
+
+	<tr>
+		<th><a href="admin/login.php"><button>Admin Login</button></a></th>
+		<th><a href="doctor/login.php"><button>Doctor Login</button></a></th>
+		<th><a href="nurse/login.php"><button>Nurse Login</button></a></th>
+		<th><a href="patient/login.php"><button>Patient Login</button></a></th>
+	</tr>
+
+</table>
+<br><br>
+
+<div id="footer">
+	Hospital Database Management System<br>
+	Created By Nathan Darby, Noah Giustini, and Erslan Salman<br><br><br>
+</div>
 </body>
 </html>
--- www/patient/login.php
+++ www/patient/login.php
@@ -1,9 +1,6 @@
 <form action="patient-login.php" method="post">
    User ID: <input type="text" name="UserID" required><br><br>
    Password: <input type="password" name="Password" required><br><br>
-   <input type="submit" value="Submit" >
-</form>
-
-<form action="../index.php" method="post">
-  <input type="button" value="Cancel">
+   <input type="submit" value="Submit" />
+   <input type="button" name = "Cancel" value="Cancel" onClick = "window.location='../index.php';"/>
 </form>
